46953acae3dc93a53319b19c1c77585a8fd5a022
Ray <ray920707@gmail.com>
2022-06-17 15:15:45 +0800

    score

---
+++ src/main/java/com/fju/score/Student.kt
@@ -0,0 +1,15 @@
+package com.fju.com.fju.score
+
+class Student(var name:String,var english:Int,var math:Int) {
+init {
+    //println("Testing")
+}
+    fun print(){
+        println("$name\t$english\t$math\t{getAverage()}")
+    }
+    fun getAverage()=(english+math/2)
+}
+fun main(){
+    val stu = Student("Jack",55,87)
+    stu.print()
+}
